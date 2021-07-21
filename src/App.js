58ab15470c4f91b9d797d2281eb5b8b701de2c7d
Arthur Hermann <arthurhermannc@gmail.com>
2021-07-21 10:56:29 -0300

    ex1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Cars from './Cars';
 
-function App() {
-  return (
-    <Cars />
-  );
+export const CarContext = React.createContext();
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      cars: {
+        red: false,
+        blue: false,
+        yellow: false,
+      },
+    }
+  }
+
+  moveCar = (car, side) => {
+    this.setState((previus) => ({
+      cars: {
+        ...previus.cars,
+        [car]: !side
+      }
+    }))
+  };
+  
+  render() {
+    const contextValue = {
+      cars: {...this.state.cars},
+      moveCar: this.moveCar,
+    }
+    return (
+      <CarContext.Provider value={contextValue}>
+        <Cars />
+      </CarContext.Provider>
+      
+    );
+  }
 }
 
 export default App;
+
